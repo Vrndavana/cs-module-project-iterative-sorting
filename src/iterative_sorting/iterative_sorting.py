36b1a2d01bdb3d7279ce3b350846f2a010c4f9f7
Vrndavana <Vrmdavama.th@gmail.com>
2020-06-15 18:55:43 -0700

    Sorting and Searching

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,40 +7,23 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
         # Your code here
-
+        if cur_index != smallest_index:
+            value = arr[smallest_index]
+            arr.remove(value)
+            arr.insert(cur_index, value)
     return arr
-
-
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
-    return arr
-
-'''
-STRETCH: implement the Count Sort function below
-
-Counting sort is a sorting algorithm that works on a set of data where
-we specifically know the maximum value that can exist in that set of
-data. The idea behind this algorithm then is that we can create "buckets"
-from 0 up to the max value. This is most easily done by initializing an
-array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
-
-Each buckets[i] then is responsible for keeping track of how many times 
-we've seen `i` in the input set of data as we iterate through it.
-Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
-
-What is the time and space complexity of the counting sort algorithm?
-'''
-def counting_sort(arr, maximum=None):
-    # Your code here
-
-
+    swaps = True
+    while swaps:
+        swaps = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swaps = True
     return arr
